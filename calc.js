133e624eadce6bc4a22d6819b02fe4aef90a1b6c
Daniel <daniel.uzeda16@gmail.com>
2019-04-05 11:38:49 -0300

    incio calculadora

---
+++ calc.js
@@ -0,0 +1,34 @@
+//Logica Calculadora
+
+operacao = 'X';
+
+function opera(op){
+    n1 = Number(document.getElementById('in1').value);
+    n2 = Number(document.getElementById('in2').value);
+
+    operacao = document.getElementById(op).innerHTML;
+    document.getElementById('lb-operacao').innerHTML = operacao;
+
+    switch(operacao){
+        case 'X':
+            resultado = n1 * n2;
+            break
+        case "/":
+            resultado = n1 / n2;
+            break
+        case "+":
+            resultado = n1 + n2;
+            break
+        case "-":
+            resultado = n1 - n2;
+            break
+    }
+    document.getElementById('out').value = resultado;
+}
+
+function limpa(){
+    document.getElementById('in1').value = "";
+    document.getElementById('in2').value = "";
+    document.getElementById('out').value = "";
+    document.getElementById('lb-operacao').innerHTML = '[]';
+}
